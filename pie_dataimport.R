6951313570ef4f3eae131937dfb6fc04b76ae34f
Jiazhou Chen <rgagechen@gmail.com>
2018-10-29 17:04:14 -0400

    update w data!

--- pie_dataimport.R
+++ pie_dataimport.R
@@ -1,14 +1,14 @@
 #Behavioral Processing for Pie Task:
 source("pie_utility.R")
-#Step 1, data importation:
+
+
+if(file.exists("pie_data.rdata")) {load("pie_data.rdata")} else {
 boxsyncpath<-"/Volumes/bek/Box Sync"
-
+boxsyncpath<-findbox()
 piedata_raw<-pie_getdata(boxsyncpath)
-
+}
 pie_data_proc<-ProcApply(piedata_raw$list,pie_preproc,filter_freechoice=T,only_firstfree=F)
 
 pie_firstfree<-ProcApply(piedata_raw$list,pie_preproc,filter_freechoice=T,only_firstfree=T)$df
 
-
-
-
+save(piedata_raw,pie_data_proc,pie_firstfree,file = "pie_data.rdata")
